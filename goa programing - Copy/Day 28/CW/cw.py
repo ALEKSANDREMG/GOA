f8a2cbb296cc7a630c37bcbbede619344b3a25ce
ALEKSANDREMG <159536199+ALEKSANDREMG@users.noreply.github.com>
2024-11-06 21:51:55 -0800

    Add files via upload

---
+++ goa programing - Copy/Day 28/CW/cw.py
@@ -0,0 +1,31 @@
+# name = input(" what your name ")
+# age = input("what your age")      #1
+# last_name =input("what yourlast_name")
+
+
+
+
+# num1 = int(input("enter first number: "))
+# num2 = int(input("enter second number: "))       2
+
+
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+
+
+# name = input("enter your name")
+# print("hi "+ name)
+
+
+print(5>3)
+print(5<3)
+print(5>=3)
+print(5<=3)
+print(5!=3)
+print(5==3)
+print()
+print()
+print()   
+print()
